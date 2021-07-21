ea9367638f2245a07d17c233430f7448d547cd3a
Juan Ojeda <juan@juanojeda.com>
2021-07-21 14:41:13 +1000

    add fonts

---
+++ src/components/Fonts.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+export const Fonts = () => (<Helmet>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="crossorigin" />
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,400;0,700;1,200;1,400;1,700&display=swap" rel="stylesheet" />
+</Helmet>);
