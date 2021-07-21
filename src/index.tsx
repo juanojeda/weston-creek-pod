b345fad72850d0a086384a44be96242d58130f29
Juan Ojeda <juan@juanojeda.com>
2021-07-21 14:41:40 +1000

    add fonts and global styles and theme to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 import Home from './Home';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyles } from './utils/GlobalStyles';
+import { theme } from './utils/theme';
+import { Fonts } from './components/Fonts';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <ThemeProvider theme={theme}>
+      <Fonts />
+      <GlobalStyles />
+      <Home />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
