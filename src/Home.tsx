68bfb1862256c831225c78454aae980dbe985a89
Juan Ojeda <juan@juanojeda.com>
2021-07-21 14:42:39 +1000

    add Header to Home

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
+import {Header} from './components/Header';
 
-function Home() {
+const Home = () => {
   return (
     <div>
-      <header>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
