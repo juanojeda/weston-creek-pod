39b3fea32a0fb25d87ed25b6b78a695b34db3718
Juan Ojeda <juan@juanojeda.com>
2021-07-21 14:39:54 +1000

    create globalstyles component

---
+++ src/utils/GlobalStyles.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
+
+export const GlobalStyles = createGlobalStyle`
+  ${normalize};
+  html {
+    font-size: 16px;
+    font-family: 'Nunito', sans-serif;
+  }
+`;
