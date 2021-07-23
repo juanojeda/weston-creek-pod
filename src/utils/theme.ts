e13ad09f45da0e1028d1bef988acd3d378555582
Juan Ojeda <juan@juanojeda.com>
2021-07-23 15:56:12 +1000

    add grid config

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,13 +1,36 @@
 import { DefaultTheme } from "styled-components";
 
+const getRemsFromPx = (px: number): number => px / 16;
+
 export const theme: DefaultTheme = {
+  awesomegrid: {
+    columns: {
+      xs: 6,
+      sm: 6,
+      md: 12,
+      lg: 16,
+      xl: 16
+    },
+    breakpoints: {
+      xs: 1,
+      sm: getRemsFromPx(600),
+      md: getRemsFromPx(900),
+      lg: getRemsFromPx(1200),
+      xl: getRemsFromPx(1200)
+    }
+  },
   iconSize: {
     sm: 3,
-    md: 4,
+    md: 4
   },
   breakpoint: {
     sm: 0,
-    md: 900,
+    md: 900
   },
-  colour: {},
+  colour: {
+    primary: "teal",
+    secondary: "darkTeal",
+    foreground: "darkGrey",
+    background: "offWhite"
+  }
 };
