7b8577ebaa7808ae8589bbef7c76ee2b1d2f5f04
Juan Ojeda <juan@juanojeda.com>
2021-07-24 22:05:00 +1000

    styling nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,19 +1,30 @@
 import React, { FunctionComponent } from "react";
 import { Col } from "react-awesome-styled-grid";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export enum Route {
-  HOME = "home",
-  OUR_TEAM = "our-team",
-  SERVICES = "services",
-  FAQS = "faqs",
-  BOOKINGS = "bookings",
-  CONTACT = "contact",
+  HOME = "#home",
+  OUR_TEAM = "#our-team",
+  SERVICES = "#services",
+  FAQS = "#faqs",
+  BOOKINGS = "//google.com",
+  CONTACT = "#contact",
 };
 
 interface NavigationProps {
   activeSection?: Route;
 };
+
+interface LinkProps {
+  readonly $external?: boolean;
+  readonly $active?: boolean
+};
+
+interface LinkConfig {
+  href: string;
+  text: string;
+  isExternal?: boolean;
+}
 
 const NavigationWrapper = styled(Col).attrs({
   as: "nav",
@@ -22,10 +33,51 @@ const NavigationWrapper = styled(Col).attrs({
   md: 3,
   lg: 3
 })`
-  outline: 1px solid red;
+  background: ${({ theme }) => theme.colour.primary};
+  border-radius: 5px;
+  padding: 1.5rem 0 2rem;
+`;
+
+const Link = styled.a <LinkProps>`
+  border-radius: 1.5rem;
+  color: ${({ theme }) => theme.colour.background};
+  display: block;
+  margin: .25rem 1rem;
+  padding: .25rem 1rem;
+
+  ${({ $external }) => $external && css`
+    ::after {
+      content: "";
+      display: inline-block;
+      outline: 1px solid red;
+      width: 1rem;
+      height: 1rem;
+      margin-left: .5rem;
+      vertical-align: baseline;
+    }
+  `}
+
+  ${({ $active }) => $active && css`
+    font-weight: bold;
+  `};
 `;
 
 export const Navigation: FunctionComponent<NavigationProps> = ({ activeSection }) => {
 
-  return (<NavigationWrapper>Nav</NavigationWrapper>);
+  const links: LinkConfig[] = [
+    { href: Route.HOME, text: "Home" },
+    { href: Route.OUR_TEAM, text: "Our team" },
+    { href: Route.SERVICES, text: "Services" },
+    { href: Route.FAQS, text: "FAQs" },
+    { href: "//google.com", text: "Bookings", isExternal: true },
+    { href: Route.CONTACT, text: "Contact us" },
+  ];
+
+  return (
+    <NavigationWrapper>
+      {links.map(({ href, text, isExternal = false }) => (
+        <Link href={href} $external={isExternal}>{text}</Link>
+      ))}
+    </NavigationWrapper>
+  );
 }
